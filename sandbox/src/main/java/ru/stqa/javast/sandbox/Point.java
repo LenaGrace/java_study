8b5015756492c4a66ac2ee634a348a53031da7d0
LenaGrace <37972887+LenaGrace@users.noreply.github.com>
2018-04-01 18:10:37 +0300

    methods added

--- sandbox/src/main/java/ru/stqa/javast/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/javast/sandbox/Point.java
@@ -1,5 +1,17 @@
 package ru.stqa.javast.sandbox;
 
 public class Point {
-    public double x, y;
+    public double x, y, x1, y1;
+
+    public Point(double x, double y, double x1, double y1) {
+        this.x = x;
+        this.y = y;
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public double distance() {
+        return  Math.sqrt((this.y1 - this.y)*(this.y1 - this.y) + (this.x1 - this.x)*(this.x1 - this.x));
+
+    }
 }
--- sandbox/src/main/java/ru/stqa/javast/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javast/sandbox/myFirstProgram.java
@@ -3,19 +3,9 @@ package ru.stqa.javast.sandbox;
 public class myFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello everyone!");
-        Point p2 = new Point();
-        Point p1 = new Point();
-        p1.x = 0.0;
-        p1.y = 0.0;
-        p2.x = 4.0;
-        p2.y = 3.0;
-        System.out.println("Расстояние между точками " + distance(p1,p2));
+        Point p1 = new Point(0.0,0.0, 3.0, 4.0);
+
+        System.out.println("Расстояние между точками " + p1.distance());
     }
 
-    public static double distance(Point p1, Point p2) {
-        double l = Math.sqrt((p2.y - p1.y)*(p2.y - p1.y) + (p2.x - p1.x)*(p2.x - p1.x));
-        return l;
-
-    }
 }
