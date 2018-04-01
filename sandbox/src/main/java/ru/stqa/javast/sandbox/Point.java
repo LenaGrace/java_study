fd0d2a391213c36ae270719e7abaef33201722cc
LenaGrace <37972887+LenaGrace@users.noreply.github.com>
2018-04-01 17:49:38 +0300

    Point class added

---
+++ sandbox/src/main/java/ru/stqa/javast/sandbox/Point.java
@@ -0,0 +1,5 @@
+package ru.stqa.javast.sandbox;
+
+public class Point {
+    public double x, y;
+}
--- sandbox/src/main/java/ru/stqa/javast/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javast/sandbox/myFirstProgram.java
@@ -1,7 +1,21 @@
 package ru.stqa.javast.sandbox;
 
 public class myFirstProgram {
-public static void main(String[] args) {
-System.out.println("Hello everyone!");
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hello everyone!");
+        Point p2 = new Point();
+        Point p1 = new Point();
+        p1.x = 0.0;
+        p1.y = 0.0;
+        p2.x = 4.0;
+        p2.y = 3.0;
+        System.out.println("Расстояние между точками " + distance(p1,p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double l = Math.sqrt((p2.y - p1.y)*(p2.y - p1.y) + (p2.x - p1.x)*(p2.x - p1.x));
+        return l;
+
+    }
 }
